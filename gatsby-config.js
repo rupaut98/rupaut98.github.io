5853c59a5935fed517715202335851f24bf0bac6
rupaut98 <rupakraut78@gmail.com>
2024-07-24 17:59:27 -0400

    Changed metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: '/logo.jpg',
       },
     },
     `gatsby-plugin-offline`,
